512c0985f779c9e4a00f356d9ec9c186bd771927
peterzky <378096232@qq.com>
2017-05-23 05:07:59 +0800

    ranger fasd

--- ranger/commands.py
+++ ranger/commands.py
@@ -152,3 +152,16 @@ class extracthere(Command):
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
+
+class fasd(Command):
+    """
+    :fasd
+
+    Jump to directory using fasd
+    """
+    def execute(self):
+        import subprocess
+        arg = self.rest(1)
+        if arg:
+            directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
+            self.fm.cd(directory)
